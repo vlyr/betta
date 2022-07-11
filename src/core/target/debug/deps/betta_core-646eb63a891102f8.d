89c0457440f2760720ec09b0810227ac3312439a
vlyr <vlyr@tuta.io>
2022-07-11 19:28:28 +0300

    rm src/core/target

--- src/core/target/debug/deps/betta_core-646eb63a891102f8.d
+++
@@ -1,6 +0,0 @@
-/home/vlyr/code/projects/betta/src/core/target/debug/deps/betta_core-646eb63a891102f8.rmeta: src/lib.rs src/error/mod.rs
-
-/home/vlyr/code/projects/betta/src/core/target/debug/deps/betta_core-646eb63a891102f8.d: src/lib.rs src/error/mod.rs
-
-src/lib.rs:
-src/error/mod.rs:
--- src/core/target/debug/deps/betta_core-6674bfbc80cf7f05.d
+++
@@ -1,6 +0,0 @@
-/home/vlyr/code/projects/betta/src/core/target/debug/deps/betta_core-6674bfbc80cf7f05.rmeta: src/lib.rs src/error/mod.rs
-
-/home/vlyr/code/projects/betta/src/core/target/debug/deps/betta_core-6674bfbc80cf7f05.d: src/lib.rs src/error/mod.rs
-
-src/lib.rs:
-src/error/mod.rs:
